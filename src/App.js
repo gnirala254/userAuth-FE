5d3584d2dca35a2e7e9aa6bb5f0725e4965d110a
Gautam Nirala <gautam.nirala@pristyncare.com>
2023-03-12 21:04:49 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const isLoggedIn = window.localStorage.getItem("loggedIn");
+
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={isLoggedIn === "true" ? <Navigate to="/home" /> : <Navigate to="/login" />}
+                    />
+                    <Route path="/login" element={isLoggedIn === "true" ? <Navigate to="/home" /> : <Login />} />
+                    <Route path="/sign-up" element={isLoggedIn === "true" ? <Navigate to="/home" /> : <SignUp />} />
+                    <Route path="/home" element={isLoggedIn === "true" ? <Home /> : <Navigate to="/login" />} />
+                    <Route path="*" element={isLoggedIn === "true" ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+export default App
